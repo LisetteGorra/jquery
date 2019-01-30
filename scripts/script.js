6efaa5149007477595872dbaaf2fa2a5c533a330
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-30 20:18:05 +0000

    Used selectors to change css

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
-    alert("jquery loaded");
-});
+$("#lead-banner p");
+
+$("h3").css({border: "3px solid blue"});
+
+$(".wrapper").css({border:"spx solid red"});
+
+$("#clients").css({border: "3px solid yellow"});
