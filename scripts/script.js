88dbdec336434d2ea64dae7f9eaa58d5935cb840
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-04 23:48:15 +0000

    Wrapped and unwrapped elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,9 @@
-//SOME CODE TO PULL IN MY LATEST TWITTER TWEET
 
-var tweet = "The big fight live: Ham vs Cheese!";
+$("section").wrap("<div>");
 
-$("#tweets div p").text(tweet);
 
-// this shows up on the bottom of the page 
-
-//ADDING & CHANGING CONTENT
-
-// .append() adds content to bottom of the element 
-// .prepend() adds content to top of element 
-// .before() adds content before element
-// .after() adds content after element
-// .html() changes the whole html of the element
-// .text() changes the text of an element
+// wrap() wraps all the matched elements individually 
+// unwrap()
 
 
 
@@ -33,6 +23,26 @@ $("#tweets div p").text(tweet);
 
 
 
+
+
+
+
+// //SOME CODE TO PULL IN MY LATEST TWITTER TWEET
+
+// var tweet = "The big fight live: Ham vs Cheese!";
+
+// $("#tweets div p").text(tweet);
+
+// // this shows up on the bottom of the page 
+
+// //ADDING & CHANGING CONTENT
+
+// // .append() adds content to bottom of the element 
+// // .prepend() adds content to top of element 
+// // .before() adds content before element
+// // .after() adds content after element
+// // .html() changes the whole html of the element
+// // .text() changes the text of an element
 
 
 // $("#contact-methods").css({border: "2px solid red"}).next().css({border: "2px solid green"}).closest("section").css({border: "2px solid blue"});
